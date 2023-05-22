f9e4b3b2982fb6343d4ffe44210dff0b0bca3e93
XIE <2719864421@qq.com>
2023-05-22 11:26:30 +0800

    实现了主页面的部分内容

--- app/src/main/java/myapplication/FragmentAA.java
+++ app/src/main/java/myapplication/FragmentAA.java
@@ -1,17 +1,75 @@
 package myapplication;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import androidx.fragment.app.Fragment;
+import android.widget.LinearLayout;
+
+import androidx.annotation.Nullable;
+
 import com.logIn.R;
 
-public class FragmentAA extends Fragment {
+public class FragmentAA extends Activity {
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_aa, container, false);
-        // 在这里对视图进行操作或添加事件监听器等
-        return view;
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_aa);
+        LinearLayout button1 = findViewById(R.id.button1);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FragmentAA.this, Classify_1.class);
+                startActivity(intent);
+            }
+        });
+        LinearLayout button2 = findViewById(R.id.button2);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FragmentAA.this, Classify_2.class);
+                startActivity(intent);
+            }
+        });
+        LinearLayout button3 = findViewById(R.id.button3);
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FragmentAA.this, Classify_3.class);
+                startActivity(intent);
+            }
+        });
+        LinearLayout button4 = findViewById(R.id.button4);
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FragmentAA.this, Classify_4.class);
+                startActivity(intent);
+            }
+        });
+        LinearLayout button5 = findViewById(R.id.button5);
+
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FragmentAA.this, Classify_5.class);
+                startActivity(intent);
+            }
+        });
+        LinearLayout button6 = findViewById(R.id.button6);
+
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FragmentAA.this, Classify_6.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
---
+++ app/src/main/java/myapplication/Searchbar.java
@@ -0,0 +1,15 @@
+package myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import com.logIn.R;
+
+public class Searchbar extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.searchbar);
+    }
+}
