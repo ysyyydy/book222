76e7cd41ecacd5eccdc323145dc8946073556984
kkkkkk <3129309628@qq.com>
2023-05-23 23:09:25 +0800

    FRAGMWNTE的设计

---
+++ app/src/main/java/fgmente/button4.java
@@ -0,0 +1,17 @@
+package fgmente;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+import com.logIn.R;
+
+public class button4 extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.button4);
+    }
+}
